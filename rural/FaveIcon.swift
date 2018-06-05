dbfe2c47546d3b326e09f2a2a78386a6ceff770a
JoaquinDiaz <joaco_diaz_ramirez@hotmail.com>
2018-06-05 01:46:54 -0700

    Boton favorito funcional con contador.

---
+++ rural/FaveIcon.swift
@@ -0,0 +1,21 @@
+//
+//  FaveIcon.swift
+//  rural
+//
+//  Created by JOAQUIN DIAZ RAMIREZ on 1/6/18.
+//  Copyright © 2018 DAVID ANGULO CORCUERA. All rights reserved.
+//
+
+import UIKit
+
+class FaveIcon: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
